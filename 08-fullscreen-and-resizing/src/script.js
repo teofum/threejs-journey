abbf93d9d4e2c062b7b72e925466597f8ebcd44e
Teo Fumagalli <teo.fumagalli@aerolab.co>
2022-08-07 15:51:18 -0300

    lesson 08-fullscreen-resizing

---
+++ 08-fullscreen-and-resizing/src/script.js
@@ -0,0 +1,94 @@
+import './style.css';
+import * as Three from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new Three.Scene();
+
+/**
+ * Object
+ */
+const geometry = new Three.BoxGeometry(1, 1, 1);
+const material = new Three.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new Three.Mesh(geometry, material);
+scene.add(mesh);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new Three.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new Three.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Handle resizing
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+// Handle fullscreen
+window.addEventListener('dblclick', () => {
+  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) canvas.requestFullscreen();
+    else canvas.webkitRequestFullscreen();
+  } else if (document.exitFullscreen) document.exitFullscreen();
+  else document.webkitExitFullscreen();
+});
+
+/**
+ * Animate
+ */
+// const clock = new Three.Clock();
+
+const tick = () => {
+  // const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
