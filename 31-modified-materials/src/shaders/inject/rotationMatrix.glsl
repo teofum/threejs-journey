05eca7b9d0a7f149266b677e4b252b58b8975526
Teo Fumagalli <teo.fumagalli@aerolab.co>
2022-10-10 17:31:49 -0300

    lesson 31-modified-materials

---
+++ 31-modified-materials/src/shaders/inject/rotationMatrix.glsl
@@ -0,0 +1,7 @@
+#include <common>
+
+uniform float uTime;
+
+mat2 get2DRotationMatrix(float _angle) {
+  return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
+}
---
+++ 31-modified-materials/src/shaders/inject/twist.base.vertex.glsl
@@ -0,0 +1,4 @@
+void main() {
+
+float angle = position.y * sin(uTime) * 0.7;
+mat2 rotationMatrix = get2DRotationMatrix(angle);
